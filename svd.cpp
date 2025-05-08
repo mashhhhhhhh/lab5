843d5bb230b9f71bf83a38cf406a4aade5248151
mashhhhhhhh <ehkorontin@mail.ru>
2025-05-09 00:20:23 +0700

    Update svd.cpp

--- svd.cpp
+++ svd.cpp
@@ -2,17 +2,71 @@
 #include <cmath>
 #include <algorithm>
 
-void jacobi_svd(const std::vector<std::vector<double>>& A,
-    std::vector<std::vector<double>>& U,
-    std::vector<double>& S,
-    std::vector<std::vector<double>>& V) {
+using namespace std;
+
+void jacobi_svd(const vector<vector<double>>& A,
+    vector<vector<double>>& U,
+    vector<double>& S,
+    vector<vector<double>>& V);
+
+vector<double> solve_svd(const vector<vector<double>>& A,
+    const vector<double>& f) {
+    int n = A.size();
+    vector<vector<double>> U, V;
+    vector<double> S;
+
+    // Вычисляем SVD
+    jacobi_svd(A, U, S, V);
+
+    // Вычисляем U^T * f
+    vector<double> ut_f(n, 0.0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            ut_f[i] += U[j][i] * f[j];
+        }
+    }
+
+    // Делим на сингулярные числа
+    vector<double> y(n, 0.0);
+    for (int i = 0; i < n; ++i) {
+        y[i] = ut_f[i] / S[i];
+    }
+
+    // Умножаем на V
+    vector<double> x(n, 0.0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            x[i] += V[i][j] * y[j];
+        }
+    }
+
+    return x;
+}
+
+vector<double> compute_svd_values(const vector<vector<double>>& A) {
+    vector<vector<double>> U, V;
+    vector<double> S;
+    jacobi_svd(A, U, S, V);
+    return S;
+}
+
+double compute_cond_number(const vector<vector<double>>& A) {
+    auto sv = compute_svd_values(A);
+    double cond = sv.front() / sv.back();
+    return cond;
+}
+
+void jacobi_svd(const vector<vector<double>>& A,
+    vector<vector<double>>& U,
+    vector<double>& S,
+    vector<vector<double>>& V) {
     const int n = A.size();
     const double eps = 1e-12;
     const int max_iter = 100;
 
     U = A;
     S.assign(n, 0.0);
-    V.assign(n, std::vector<double>(n, 0.0));
+    V.assign(n, vector<double>(n, 0.0));
     for (int i = 0; i < n; ++i) V[i][i] = 1.0;
 
     for (int iter = 0; iter < max_iter; ++iter) {
@@ -47,18 +101,5 @@ void jacobi_svd(const std::vector<std::vector<double>>& A,
     }
 
     for (int i = 0; i < n; ++i) S[i] = fabs(U[i][i]);
-    std::sort(S.begin(), S.end(), std::greater<double>());
-}
-
-std::vector<double> compute_svd_values(const std::vector<std::vector<double>>& A) {
-    std::vector<std::vector<double>> U, V;
-    std::vector<double> S;
-    jacobi_svd(A, U, S, V);
-    return S;
-}
-
-double compute_cond_number(const std::vector<std::vector<double>>& A) {
-    auto sv = compute_svd_values(A);
-    if (sv.empty() || sv.back() < 1e-12) return 1.0;
-    return sv.front() / sv.back();
+    sort(S.begin(), S.end(), greater<double>());
 }
